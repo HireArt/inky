ea2287ff855065dfe9ba4dbc5d6b83f0e20ede42
Smudge <nathan@ngriffith.com>
2015-02-11 15:46:57 -0500

    Explicitly request metadata fields (since md5 doesn't come by default)

--- lib/inky/file.rb
+++ lib/inky/file.rb
@@ -5,6 +5,7 @@ module Inky
   class File
     METADATA_FIELDS = :md5, :mimetype, :uploaded, :container, :writeable,
                       :filename, :location, :key, :path, :size
+    MF_ARGS = METADATA_FIELDS.each_with_object({}) { |f, h| h[f] = true }
 
     attr_accessor(*METADATA_FIELDS)
     attr_accessor :uid, :local_file, :remote_url, :metadata
@@ -16,23 +17,23 @@ module Inky
         self.remote_url = value
       else
         self.uid = value
-        reload_file
+        request_metadata
       end
     end
 
   private
 
-    def reload_file
+    def request_metadata
       return unless uid
-      response = RestClient.get "#{API_BASE_URL}/file/#{uid}/metadata"
+      response = RestClient.get "#{API_BASE_URL}/file/#{uid}/metadata",
+                                params: MF_ARGS
       self.metadata = JSON.parse(response)
-      METADATA_FIELDS.each do |f|
-        send("#{f}=", metadata[f.to_s])
-      end
+      METADATA_FIELDS.each { |f| send("#{f}=", metadata[f.to_s]) }
     end
 
     def valid_url?(url)
-      (parsed = Addressable::URI.parse(url)) && %w(http https).include?(parsed.scheme)
+      (parsed = Addressable::URI.parse(url)) &&
+        %w(http https).include?(parsed.scheme)
     rescue Addressable::URI::InvalidURIError
       false
     end
