c44c2a856b69fae6c712ecb14d4f6f973a7dc2df
Smudge <nathan@ngriffith.com>
2015-02-12 13:37:15 -0500

    Adding a bunch of metadata tests.

--- spec/metadata_spec.rb
+++ spec/metadata_spec.rb
@@ -3,13 +3,57 @@ require 'support/spec_helper'
 API_BASE_URL = 'https://www.filepicker.io/api'
 
 describe Inky::File do
-  describe '.new' do
+  context 'when reading existing file' do
     let(:uid) { 'hFHUCB3iTxyMzseuWOgG' }
-    let(:url) { "#{API_BASE_URL}/file/#{uid}" }
-    subject { Inky::File.new(uid) }
+    let(:file) { Inky::File.new(uid) }
 
-    context 'when valid' do
-      it { expect(subject.url).to eq(url) }
+    describe '#url' do
+      subject { file.url }
+      it { is_expected.to eq("#{API_BASE_URL}/file/#{uid}") }
+    end
+    describe '#size' do
+      subject { file.size }
+      it { is_expected.to eq(175_210) }
+    end
+    describe '#mimetype' do
+      subject { file.mimetype }
+      it { is_expected.to eq('image/png') }
+    end
+    describe '#uploaded' do
+      subject { file.uploaded }
+      it { is_expected.to eq(1_390_334_370_000.0) }
+    end
+    describe '#container' do
+      subject { file.container }
+      it { is_expected.to eq('your_own_bucket') }
+    end
+    describe '#writeable' do
+      subject { file.writeable }
+      it { is_expected.to eq(false) }
+    end
+    describe '#filename' do
+      subject { file.filename }
+      it { is_expected.to eq('5qYoopVTsixCJJiqSWSE.png') }
+    end
+    describe '#location' do
+      subject { file.location }
+      it { is_expected.to eq('S3') }
+    end
+    describe '#key' do
+      subject { file.key }
+      it { is_expected.to eq('ZynOv436QOirPYbJIr3Y_5qYoopVTsixCJJiqSWSE.png') }
+    end
+    describe '#path' do
+      subject { file.path }
+      it { is_expected.to eq('ZynOv436QOirPYbJIr3Y_5qYoopVTsixCJJiqSWSE.png') }
+    end
+    describe '#width' do
+      subject { file.width }
+      it { is_expected.to eq(400) }
+    end
+    describe '#height' do
+      subject { file.height }
+      it { is_expected.to eq(400) }
     end
   end
 end
